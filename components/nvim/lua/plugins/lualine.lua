b734fce92a59218eff272286a4e97b0e67ef2d9c
Robert Ingrum <robert.ingrum@shopify.com>
2026-06-23 12:14:22 -0600

    Update lualine to include relative path.

--- components/nvim/lua/plugins/lualine.lua
+++ components/nvim/lua/plugins/lualine.lua
@@ -2,11 +2,16 @@
 
 return {
 	'nvim-lualine/lualine.nvim',
-	opts = {
-		options = {
-			icons_enabled = true,
-			theme = 'tokyonight',
+	enabled = true,
+	opts = function(_, opts)
+		opts.icons_enabled = true
+		opts.theme = 'auto'
+		opts.sections = {
+			lualine_c = { { "filename", path = 1 } },
+			lualine_x = { "encoding", "filetype", "lsp_status" } -- I removed "encoding" here and added "lsp_status"
 		}
-	},
+
+		return opts
+	end,
 }
 
